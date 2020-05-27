11c7fe7fe18ebd2a664249b9e290f6243ca3902a
voronenko-p <vyacheslav@voronenko.info>
2020-05-27 12:50:08 +0200

    Adjusted dlls (#1)
    * chore(deps) moved to local assembly artifacts
    
    * chore(starttime in nanoseconds)
    
    * improve(webconfig) reordered lines in webconfig a bit
    
    * chore(demo) support for disabling enrichment on startup + some logging
    
    * chore(readme)
    
    * chore(registry) inproc COM registration due to missing DLL register points
    
    * chore(regedit) workaround on missing Register/Unregister server entries in native.dll
    
    * adjusted web config
    
    Co-authored-by: Gitlab runner <gitlab@runner.some>

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,10 @@
+using Datadog.Trace;
+using Datadog.Trace.Configuration;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -12,6 +15,27 @@ namespace Samples.AspNetMvc4.Controllers
 {
     public class HomeController : Controller
     {
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+/*
+            // access the active scope through the global tracer (can return null)
+            var scope = Tracer.Instance.ActiveScope;
+
+            Process currentProcessInfo = System.Diagnostics.Process.GetCurrentProcess();
+            var startTime = currentProcessInfo.StartTime;
+            TimeSpan startTimeSpan = (startTime.ToUniversalTime() - new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+            long startTimeNanoSeconds = startTimeSpan.Ticks * 100;
+
+
+            
+            // add a tag to the span
+            scope.Span.SetTag("span.pid", currentProcessInfo.Id.ToString());
+            scope.Span.SetTag("span.starttime", startTimeNanoSeconds.ToString());
+            scope.Span.SetTag("span.hostname", Environment.MachineName);
+*/
+            base.OnActionExecuting(filterContext);
+        }
 
         public static string FullyQualifiedApplicationPath(HttpRequestBase httpRequestBase)
         {
@@ -37,7 +61,7 @@ namespace Samples.AspNetMvc4.Controllers
 
         public ActionResult Index()
         {
-            var prefixes = new[] { "COR_", "CORECLR_", "DD_", "DATADOG_" };
+            var prefixes = new[] { "COR_", "CORECLR_", "DD_", "DATADOG_", "foo" };
 
             var envVars = from envVar in Environment.GetEnvironmentVariables().Cast<DictionaryEntry>()
                           from prefix in prefixes
@@ -47,12 +71,29 @@ namespace Samples.AspNetMvc4.Controllers
                           orderby key
                           select new KeyValuePair<string, string>(key, value);
 
+            Process currentProcessInfo = System.Diagnostics.Process.GetCurrentProcess();
+            var startTime = currentProcessInfo.StartTime;
+
+            TimeSpan startTimeSpan = (startTime.ToUniversalTime() - new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+            var startTimeMilliseconds = Convert.ToUInt64(Math.Truncate(startTimeSpan.TotalMilliseconds));
+
+            ViewBag.ProcessID = currentProcessInfo.Id;
+            ViewBag.startTime = startTimeMilliseconds;
+            ViewBag.machineName = Environment.MachineName;
+
             return View(envVars.ToList());
         }
 
         public ActionResult Send()
         {
-            return Content(string.Format("Hi there from sender! ({0})", DateTime.Now));
+            // extract propagated http headers
+            // var headers = httpContext.Request.Headers.Wrap();
+            // propagatedContext = SpanContextPropagator.Instance.Extract(headers);
+
+            string headers = String.Empty;
+            foreach (var key in Request.Headers.AllKeys)
+                headers += key + "=" + Request.Headers[key] + "<br/>" + Environment.NewLine;
+            return Content(string.Format("[SEND] {0} Hi there from Send() on box {1}! (Answered {2})", headers, Environment.MachineName, DateTime.Now));
         }
 
         public ActionResult Receive()
@@ -74,7 +115,43 @@ namespace Samples.AspNetMvc4.Controllers
                  responseText = reader.ReadToEnd();
             }
 
-            return Content(String.Format("Hello from consumer - in response to - {0}", responseText));
+            return Content(String.Format("[RECEIVE] from {0}/Home/Send" + "<br/>" + Environment.NewLine + "{1}", clientUrl, responseText));
+        }
+
+        public ActionResult ReceiveWrapped()
+        {
+            string responseText = String.Empty;
+            string clientUrl = ConfigurationManager.AppSettings["SENDER_URL"];
+            if (String.IsNullOrEmpty(clientUrl))
+            {
+                clientUrl = HomeController.FullyQualifiedApplicationPath(HttpContext.Request);
+            }
+
+            string externalServiceName = ConfigurationManager.AppSettings["SENDER_NAME"];
+            if (String.IsNullOrEmpty(externalServiceName))
+            {
+                externalServiceName = HomeController.FullyQualifiedApplicationPath(HttpContext.Request);
+            }
+
+            var currentServiceName = ConfigurationManager.AppSettings["DD_SERVICE_NAME"];
+
+
+            using (var scope = Tracer.Instance.StartActive("external_service_call", serviceName: externalServiceName))
+            {
+
+                HttpWebRequest webReq = (HttpWebRequest)WebRequest.Create(string.Format("{0}/Home/Send", clientUrl));
+                webReq.Method = "GET";
+                HttpWebResponse webResponse = (HttpWebResponse)webReq.GetResponse();
+
+                WebHeaderCollection header = webResponse.Headers;
+                var encoding = ASCIIEncoding.ASCII;
+                using (var reader = new System.IO.StreamReader(webResponse.GetResponseStream(), encoding))
+                {
+                    responseText = reader.ReadToEnd();
+                }
+            }
+            var finalResponse = String.Format("[RECEIVE] {0}/Home/Send" + "<br/>" + Environment.NewLine + "{1}", clientUrl, responseText);
+            return Content(finalResponse);
         }
 
         public ActionResult About()
--- Global.asax.cs
+++ Global.asax.cs
@@ -1,5 +1,10 @@
+using Datadog.Trace;
+using Datadog.Trace.Configuration;
+using log4net;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
@@ -17,6 +22,48 @@ namespace Samples.AspNetMvc4
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            log4net.Config.XmlConfigurator.Configure();
+
+            var settings = TracerSettings.FromDefaultSources();
+
+            settings.Integrations["AdoNet"].Enabled = false;
+            settings.Integrations["AspNet"].Enabled = true;
+            settings.Integrations["AspNetMvc"].Enabled = true;
+            settings.Integrations["AspNetWebApi2"].Enabled = true;
+            settings.Integrations["Wcf"].Enabled = true;
+            settings.Integrations["HttpMessageHandler"].Enabled = true;
+            settings.Integrations["WebRequest"].Enabled = true;
+                        
+            Process currentProcessInfo = System.Diagnostics.Process.GetCurrentProcess();
+            var startTime = currentProcessInfo.StartTime;
+
+            TimeSpan startTimeSpan = (startTime.ToUniversalTime() - new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+            var startTimeMilliseconds = Convert.ToUInt64(Math.Truncate(startTimeSpan.TotalMilliseconds));
+
+            ILog log = log4net.LogManager.GetLogger(typeof(Tracer));
+            log.Info($"Starting... {currentProcessInfo.Id.ToString()}");
+
+            var enrichMode = ConfigurationManager.AppSettings["STS_ENRICH_MODE"];
+            if (string.IsNullOrEmpty(enrichMode) || enrichMode != "OFF")
+            {
+                log.Info("Enriching with pid starttime hostname");
+                settings.GlobalTags.Add("span.pid", currentProcessInfo.Id.ToString());
+                settings.GlobalTags.Add("span.starttime", startTimeMilliseconds.ToString());
+                if (!settings.GlobalTags.ContainsKey("span.hostname"))
+                {
+                    settings.GlobalTags.Add("span.hostname", Environment.MachineName);
+                }
+            } else
+            {
+                log.Info("SKIPPED enrichment on init.");
+            }
+
+            // create a new Tracer using these settings
+            var tracer = new Tracer(settings);
+
+            // set the global tracer
+            Tracer.Instance = tracer;
         }
     }
 }
